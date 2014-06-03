df4ed602c3f591f675f618c19c1178373d390e52
Arnaud Legout <arnaud.legout@inria.fr>
2014-06-03 17:12:33 +0200

    split the semaine2-1* to move all the float discussion and math module to semaine2-2
    initial commit for semaine2-2*

--- semaine2/semaine2-1_exo1.py
+++ semaine2/semaine2-1_exo1.py
@@ -87,15 +87,3 @@ b = False
 
 print a, b
 
-## Python est livré avec pleins de boîtes à outils (on appelle
-## cela des modules).
-## Chargeons la boîte à outils math
-import math
-
-## Regardons ce qu'il y a dedans.
-dir(math)
-
-## Et utilisons quelques outils
-math.tan(math.pi/4)
-
-
