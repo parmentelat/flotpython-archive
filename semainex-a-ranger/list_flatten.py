c9ca998730036de048cba34095082365e068c198
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-06-18 10:09:21 +0200

    make list_flatten an exo of its own

--- semainex-a-ranger/list_flatten.py
+++ semainex-a-ranger/list_flatten.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
-inputs=[]
-inputs.append ( [ 'a', 'b', 'c' ] )
-inputs.append ( xrange(4) )
-inputs.append ( [ 1, 2, [3, [4, [5, 6] ,7] ,8], 9] )
+input1= [ 'a', 'b', 'c' ]
+# works with an iterator as well
+input2 = xrange(4)
+# a random tree
+input3 = [ 1, 2, [3, [4, [5, 6] ,7] ,8], 9]
+# generating lists
+input4 = [ [ n**2, n**2+1 ] for n in range (1,6) ]
+# a list of turtle moves
+input5 = [ [ ('forward', 100,), ('left', 120,) ] for i in xrange(3) ]
+
+inputs = [ input1, input2, input3, input4, input5 ]
 
 def list_flatten (list_of_lists):
     result=[]
@@ -16,7 +23,8 @@ def list_flatten (list_of_lists):
 
 def main ():
     for input in inputs:
-        print 20*'=','input:', input
-        print 'iterative:', list_flatten (input)
+        margin = len('flattened')-len('input')-1
+        print margin*'=','input:', input
+        print 'flattened:', list_flatten (input)
 
 if __name__ == '__main__': main()
