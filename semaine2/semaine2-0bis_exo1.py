371946654596688dd4b9fe618a00c90edd086453
Arnaud Legout <arnaud.legout@inria.fr>
2014-07-10 19:23:43 +0200

    creation semaine 0bis pour l'introduction des notions de base nécessaire à l'écriture de petits programmes
    semaine 0 : DONE
    semaine 0bis : en cours

--- semaine2/semaine2-0bis_exo1.py
+++ semaine2/semaine2-0bis_exo1.py
@@ -1,30 +1,35 @@
 # -*- coding: cp1252 -*-
 
-## Comment manipule-t-on des objets en Python ?
-## On utilise l'affectation (typage dymamique).
-## On dit que l'on affecte un nom à un objet ou que
-## le nom référence l'objet. Parler du typage dynamique.
-
-# abcdefghijklmnopqrstuvwxyz0123456789_
-prix_articles = 10
-
-## on ne commence pas par un chiffre
-
-1prix = 10
-
-## on ne met pas de caractères accentués dans un nom de variable
-
-carré_val = 8
-
 ## types de base en python
+## les types numériques
 val_int = 1
 val_float = 3.5 # attention, la virgule décimale est un point un Python
+
+## on a les opérations numériques classiques, Python fait automatiquement
+## les convertions si nécessaire
+
+print val_int + val_float
+print val_int - val_float
+print val_int * val_float
+print val_int / val_float
+
+## le type chaîne de catactères
 val_str = 'une chaine'
+
+## le type liste
 val_list = [] # une liste vide, suite quelconque d'éléments indexés de 0 à n-1
 val_list = [val_int, val_float, val_str]
 
 print val_list[0] # premier élément de la liste
 print val_list[2]
+
+## l'opérateur + est définit pour les chaînes de caractères et les listes
+## il retourne un nouvel objet de même type qui est la concaténation
+## des deux objets initiaux
+
+print 'spam' + 'ham'
+
+print [1, 2] + [3, 4]
 
 ## la liste est une structure de donnée très puissante et au coeur
 ## de tous les programmes Python. On verra plus tard cette semaine
