2465e394189a8451cf10906a43b12ccb49845700
Arnaud Legout <arnaud.legout@inria.fr>
2014-07-16 11:23:34 +0200

    -j'ai enlevé les booléens de la première vidéo sur les types numériques (j'en parlerai avec le if) -améliorations diverses

--- semaine2/CO12AL-W2-EXO2.py
+++ semaine2/CO12AL-W2-EXO2.py
@@ -1,8 +1,9 @@
 # -*- coding: latin_1 -*-
 
-## On va voir dans cette vidéo les 5 types numériques en Python, les types entier
-## int et long, le type décimal float, le type complex pour les nombres complexes
-## et le type bool pour le booleens. 
+## Je vous rappelle que Python utilise le typage dynamique, c'est-à-dire
+## que l'on ne donne jamais le type d'un objet à l'écriture du programme,
+## ce type est déterminé à l'execution. Ça simplifie beaucoup l'écriture
+## des programmes.
 
 ## le type entier
 ## pour entrer un entier, on n'a rien d'autre à faire que d'écrire
@@ -12,29 +13,14 @@
 ## on peut également l'affecter à une variable
 i = 1
 
-## Comment affiche-t-on un objet en Python ?
-## On utilise print (on peut séparer les variables
-## par des virgules).
-
-print i
-
-## lorsque l'on est dans le terminal interactif, on peut aussi directement
-## taper le nom de la variable suivi d'un retour chariot.
-
-i
-
-## On voit ainsi la représentation interne de l'objet ?
-## c'est souvent équivallent à print (mais pas toujours).
-## La représentation internet peut donner des informations
-## supplémentaires.
-
 ## Comment on connait le type d'un objet en Python ?
-## On utilise la fonction built-in type()
+## On utilise la fonction built-in type() qui accepte comme
+## argument une variable ou un objet. 
 
 type(i)
 
 ## en python on a deux type entiers, les int et les long
-i = 10
+i = 10 ## codé sur 32 bits sur une machine 32 bits
 l = 23480284028402840289482184018 # précision illimitée
 print l * l     # précision illimitée sur les long
 
@@ -52,9 +38,14 @@ type(i + l)
 
 ## Les décimaux. On sépare la partie entière et décimale par un .
 f = 4.3
+
+## les float sont en général équivallent aux double en C,
+## c'est à dire que ce sont des nombres codés sur 53 bits
+## avec un maximum de 15 à 17 chiffres significatifs
+
 c = 1 + 3j
 
-print f, c, c.real, c.imag
+print c.real, c.imag
 
 
 ## Par contre on peut perdre en précision.
@@ -64,13 +55,10 @@ print f, c, c.real, c.imag
 ## donne toujours un complex
 
 print i + l
-type(i+l)
 
 print i + l + f
-print type(i + l + f)
 
 print i + l + f + c
-print type(i + l + f + c)
 
 ## opérations de base
 
@@ -80,6 +68,7 @@ print -3
 print 5/3       # division entière
 print 5%3       # reste de la division entière
 print 5/3.0     # division sur des floats
+print 5/float(3)
 print 5.2//3.1  # force la division sur des entiers (5.0/3.0)
 print 2 ** 32   # puissances
 print abs(-5.3) # valeur absolue
@@ -92,10 +81,5 @@ print long(5.3)
 print float(9879729572895792375948)
 print complex(10)
 
-## Finissons par les bool
 
-a = True
-b = False
-
-print a, b
 
