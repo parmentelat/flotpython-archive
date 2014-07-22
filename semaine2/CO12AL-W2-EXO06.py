b5c8c554a2e0f8e9ea7250d8cc50b8e8392bf8b9
Arnaud Legout <arnaud.legout@inria.fr>
2014-07-22 16:56:55 +0200

    -started part on for and functions

---
+++ semaine2/CO12AL-W2-EXO06.py
@@ -0,0 +1,57 @@
+# -*- coding: iso-8859-15 -*-
+
+## prenons un problème simple, je veux afficher la liste des entiers
+## de 1 à 10 et de leur carré.
+print 'x =', 1, ', x**2 =', 1**2
+print 'x =', 2, ', x**2 =', 2**2
+print 'x =', 3, ', x**2 =', 3**2
+
+## on se rend vite compte que l'on fait 10 fois la même tache avec
+## une petite variation, la valeur de x. Les boucles for existe justement
+## pour factoriser ce type de taches. Regardons comme on écrit une boucle for
+
+for x in range(1,11):
+    print 'x =', x, ', x**2 =', x**2
+print "on est sorti de la boucle"
+## La boucle for utilise (comment souvent en Python), une notation
+## simple et intuitive. On commence une boucle for par l'instruction
+## for puis, on spécifie une variable (x dans notre cas), l'instruction
+## in, et une séquence. On fini la ligne avec un : ce qui veut
+## dire que l'on va avoir un nouveau bloc d'instruction décalé
+## de 4 caractères vers la droite par rapport au for.
+## La boucle for va répéter le bloc d'instruction autant fois qu'il
+## y a d'éléments dans la séquence. À la premiere exécution du bloc
+## d'instruction, la variable x référence le premier élément de la séquence
+## et à chaque nouvelle répétition du bloc d'instruction x référencera
+## l'élément suivant dans la séquence jusq'au dernier élément de la séquence. 
+## Lorsqu'il n'y a plus d'élément dans la séquence on sort de la boucle
+## for, c'est-à dire que l'on continu avec le bloc de code aligné avec
+## la boucle for.
+
+## regardons maintenant un autre problème. Je veux a différents moments
+## de mon programme faire une opération, par exemple, afficher
+## sur la sortie standard tous les éléments d'une liste et leur carré.
+
+L1 = [1, 4, 6, 7, 10, 11, 30, 50]
+for x in L1:
+    print 'x =', x, ', x**2 =', x**2
+
+L2 = [3.4, 11, 22, 150.435, 18]
+for x in L2:
+    print 'x =', x, ', x**2 =', x**2
+
+## on voit que grace à la boucle for, je peux factoriser mon code, mais
+## que malgré tout je répète deux fois exactement la même boucle for.
+## Un moyen de factoriser encore plus ce code est d'utiliser ce que l'on
+## appelle une fonction. Regardons comment on écrit une fonction
+
+def f(L):
+    for x in L:
+        print 'x =', x, ', x**2 =', x**2
+
+## une fonction commence avec l'instruction def, on donne ensuite un nom
+## à la fonction (f ici). puis en met entre paranthèses l'argument de la
+## fonction (L dans notre cas). 
+
+f(L1)
+f(L2)
