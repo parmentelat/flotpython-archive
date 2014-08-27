00a50ea700b9a3f9c46fecd6e6443c0cab481fbd
Arnaud Legout <arnaud.legout@inria.fr>
2014-08-27 15:12:10 +0200

    inital commit of the exception video (on-going)

---
+++ semaine4/CO12AL-W4-VIDEO11-EXO01.py
@@ -0,0 +1,16 @@
+# -*- coding: iso-8859-15 -*-
+
+## Commençons par générer une exeption
+
+#1/0
+
+## On voit que l'exception a un nom spécifique et
+## contient un message d'explication. C'est toujours
+## le cas pour toutes les exceptions built-in
+
+## Supposons maintenant que l'on fasse une fonction
+## qui divise deux nombres
+
+def f(a, b):
+    return a / b
+
