27e78410331221b8f9916e232c3bd9c03c49337b
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-08-27 08:35:53 +0200

    nbindex.py clcule un index des notebooks attendus en prerun sur connect.inria.fr

---
+++ nbindex.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from glob import glob
+
+skel_pre = "<html><body><ul>\n"
+skel_post = "</ul></body></html>\n"
+url_format = "<li><a href='https://connect.inria.fr/ipythonExercice/CoursPythonS{week}/{name}.ipynb/pthierry'>S{week} - {name}</a></li>\n"
+
+
+def main ():
+    with open ("connect.html", "w") as f:
+        f.write(skel_pre)
+        for ipynb in glob ("semaine[0-9]*/*.ipynb"):
+            dir,file = ipynb.split('/')
+            week=dir.replace('semaine','')
+            name=file.replace('.ipynb','')
+            f.write (url_format.format(week=week,name=name))
+        f.write(skel_post)
+
+if __name__ == '__main__':
+    main()
