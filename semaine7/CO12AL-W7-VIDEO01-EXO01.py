d0d9179cdc6eddf1ddc33e577ad1c3d111e93d77
Arnaud Legout <arnaud.legout@inria.fr>
2014-08-28 19:02:20 +0200

    modified W3-VIDEO4 to stress the notion mutable and immutable on-going work on W7-VIDEO1

--- semaine7/CO12AL-W7-VIDEO01-EXO01.py
+++ semaine7/CO12AL-W7-VIDEO01-EXO01.py
@@ -1,3 +1,58 @@
 # -*- coding: iso-8859-15 -*-
 
-## 
+## Supposons que l'on veuille compter le nombre d'instances
+## créées par une classe. On peut facilement maintenir un
+## compteur du nombre de référence dans contructeur de la
+## classe, mais l'accès à ce compteur n'est pas trivial.
+## Regardons cela...
+
+
+class C:
+    nb_i = 0
+    def __init__(self):
+        C.nb_i  = C.nb_i + 1
+
+
+C()
+C()
+## je peux évidamment accéder à l'attribut de la
+## classe directement. 
+print C.nb_i
+
+## Je peux aussi faire une fonction dans mon module
+
+def f():
+    return C.nb_i
+
+
+print f()
+
+## Mais cela complique la maintenance d'avoir une fonction
+## travaillant sur une classe en dehors de la classe.
+## Par contre, je ne peux pas faire une méthode d'instance
+## dans ma classe puisqu'il me faut une instance pour accéder
+## au compteur, et que cette instance va changer le compteur
+## à sa création.
+
+## La solution ici est d'avoir une méthode statique.
+## une méthode statique s'écrit comme une fonction,
+## mais doit être déclarée comme statique avec la
+## fonction staticmethod. 
+
+class C:
+    nb_i = 0
+    def __init__(self):
+        C.nb_i  = C.nb_i + 1
+    def f():
+        return C.nb_i
+    f = staticmethod(f)
+
+C()
+C()
+print C.f()
+
+## notons qu'une méthode statique peut aussi être appelée sur
+## une instance, mais que l'instance n'est pas passée à la
+## méthode
+
+print C().f()
