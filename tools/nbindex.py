22253274fd37acf118d321b6907752211c4e5d88
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-09-17 19:42:19 +0200

    update the tools that computes connect.html with the new URL to access the connect website

--- tools/nbindex.py
+++ tools/nbindex.py
@@ -2,9 +2,11 @@
 
 from glob import glob
 
+user="pthierry"
+
 skel_pre = "<html><body><ul>\n"
 skel_post = "</ul></body></html>\n"
-url_format = "<li><a href='https://connect.inria.fr/ipythonExercice/CoursPythonS{week}/{name}.ipynb/pthierry'>S{week} - {name}</a></li>\n"
+url_format = "<li><a href='https://connect.inria.fr/ipythonExercice/W{week}/{name}.ipynb/{user}'>W{week} - {name}</a></li>\n"
 
 
 def main ():
@@ -12,9 +14,9 @@ def main ():
         f.write(skel_pre)
         for ipynb in glob ("W[0-9]*/*.ipynb"):
             dir,file = ipynb.split('/')
-            week=dir.replace('semaine','')
+            week=dir.replace('W','')
             name=file.replace('.ipynb','')
-            f.write (url_format.format(week=week,name=name))
+            f.write (url_format.format(week=week,name=name,user=user))
         f.write(skel_post)
 
 if __name__ == '__main__':
