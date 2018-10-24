8b38ba73464276284f6d287d76710dc387a63a33
parmentelat <thierry.parmentelat@inria.fr>
2018-10-24 19:49:04 +0200

    nbcustomexec knows about the following cell metadata tags latex:skip-eval				: if true, cell is not executed latex:hidden-code-instead     : code executed instead of the cell code latex:hidden-code-before      : code injected before the cell code latex:hidden-code-after         : code injected after the cell code ---- most notebooks from the course have been instrumented with these tags, and could be executed in an attempt to streamline our pdf production

--- tools/nbcustomexec.py
+++ tools/nbcustomexec.py
@@ -15,32 +15,45 @@ magic)
 
 """
 
-# status right now:
-#
-# (*) a code cell that contains any of the IGNORE_IF_PRESENT strings
-#     in its source gets ignored altogether (not evaluated at all)
-# (*) a code cell that contains any of the IGNORE_IF_PRESENT_IN_METADATA
-#     tag with value among the ones defined in IGNORE_IF_PRESENT_IN_METADATA
-#     gets ignored as well
-
-
-IGNORE_IF_PRESENT_IN_SOURCE = [
-    '%ipythontutor',
-]
-
-IGNORE_IF_PRESENT_IN_METADATA = {
-    'latex:ignore' : [True],
-}
-
-
-DEBUG=False
-
 from pathlib import Path
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
+
+##########
+# status so far:
+#
+# (*) a code cell that contains any of the IGNORE_IF_PRESENT strings
+#     in its source gets ignored altogether (not evaluated at all)
+# (*) a code cell that contains any of the IGNORE_IF_PRESENT_IN_METADATA
+#     tag with value among the ones defined in IGNORE_IF_PRESENT_IN_METADATA
+#     gets ignored as well
+# (*) a cell that has a metadata tagged with the tag defined here as
+#     CODE_TO_EXEC_INSTEAD: we execute the metadata value instead of the
+#     cell source; this typically for cells that use input()
+
+IGNORE_IF_PRESENT_IN_SOURCE = [
+    '%ipythontutor',
+    # cells about auto-evaluated exercises
+    '.correction(',
+]
+
+IGNORE_IF_PRESENT_IN_METADATA = {
+    'latex:skip-eval' : [True],
+}
+
+# if this key is set in metadata, we evaluate that code
+# instead of the cell's code
+CODE_TO_EXEC_INSTEAD = "latex:hidden-code-instead"
+CODE_TO_EXEC_BEFORE = "latex:hidden-code-before"
+CODE_TO_EXEC_AFTER = "latex:hidden-code-after"
+
+# all annotations will mention this string
+MARKER = "automatic execution"
+
+##########
 
 # helpers
 def load_notebook(path: Path):
@@ -57,38 +70,66 @@ class CustomExecPreprocessor(ExecutePreprocessor):
 
     def preprocess_cell(self, cell, resources, cell_index):
 
-        ignore_it = False
+        # even if we skip a cell, we need to comply with the protocol
+        # implemented in the superclass, which expects
+        ignored_result = cell, resources
+        def mark_ignored(cell):
+            cell.source = (f"# NOTE\n"
+                           f"# {MARKER} has skipped execution of this cell\n\n"
+                           + cell.source)
+
+        substituted_code = None
 
         source = cell.source
         for ignore in IGNORE_IF_PRESENT_IN_SOURCE:
             if ignore in source:
-                ignore_it = True
-                break
+                mark_ignored(cell)
+                return ignored_result
 
         metadata = cell.metadata
         for key, value in metadata.items():
+
             if (key in IGNORE_IF_PRESENT_IN_METADATA
                     and value in IGNORE_IF_PRESENT_IN_METADATA[key]):
-                ignore_it = True
-                break
+                mark_ignored(cell)
+                return ignored_result
 
-        if DEBUG:
-            print(f"evaluating cell #{cell_index} -> {ignore_it} "
-                  f"type={type(cell.source)}")
-        if not ignore_it:
-            return ExecutePreprocessor.preprocess_cell(
-                self, cell, resources, cell_index)
-        # otherwise even if we skip that cell we need
-        # to comply with the protocol implemented in the superclass
-        return cell, resources
+            if key == CODE_TO_EXEC_INSTEAD:
+                substituted_code = cell.source
+                cell.source = metadata[key]
 
+            if key == CODE_TO_EXEC_BEFORE:
+                substituted_code = cell.source
+                cell.source = metadata[key] + "\n" + cell.source
+
+            if key == CODE_TO_EXEC_AFTER:
+                substituted_code = cell.source
+                cell.source = cell.source + "\n" + metadata[key]
+
+
+        execution_result = ExecutePreprocessor.preprocess_cell(
+            self, cell, resources, cell_index)
+
+        if substituted_code is not None:
+            cell.source = (substituted_code
+                           + f"\n\n# NOTE:\n# {MARKER} has used instead:"
+                           + f"\n##########"
+                           + f"\n{cell.source}"
+                           + f"\n##########"
+                           )
+
+        return execution_result
 
 # main
 
 def main():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-d", "--exec-dir", default="executing",
-                        help="Directory to execute and store notebooks in")
+    parser.add_argument("-v", "--verbose", default=False, action='store_true',
+                        help="List notebook names as they are taken care of")
+    parser.add_argument("-e", "--exec-dir", default=".",
+                        help="Directory to execute in")
+    parser.add_argument("-o", "--output-dir", default="executing",
+                        help="Directory to store notebooks in")
     parser.add_argument("notebooks", nargs='+')
 
     args = parser.parse_args()
@@ -102,11 +143,21 @@ def main():
         exit(1)
     resources = {'metadata': {'path': args.exec_dir}}
 
+    output_path = Path(args.output_dir)
+    if not output_path.is_dir():
+        print(f"Creating {output_path}")
+        output_path.mkdir()
+    if not output_path.is_dir():
+        print(f"Could not create output dir {output_path} - exiting")
+        exit(1)
+
     for notebook in args.notebooks:
         path = Path(notebook)
         notebook = load_notebook(path)
         stem = path.stem
-        output = exec_path / f"{stem}.ipynb"
+        output = output_path / f"{stem}.ipynb"
+        if args.verbose:
+            print(f"{path} -> {output}")
         preprocessor = CustomExecPreprocessor(timeout=600, kernel_name='python3')
         executed, resources = preprocessor.preprocess(notebook, resources)
         save_notebook(executed, output)
